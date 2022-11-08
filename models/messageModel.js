b598046f6c324c68d1b402f1fbac42a78047386c
rikifujihara <rikifujihara@gmail.com>
2022-11-08 12:25:50 +1100

    updating population fields for getMessages

--- models/messageModel.js
+++ models/messageModel.js
@@ -1,11 +1,17 @@
 const mongoose = require("mongoose");
 
-const messageSchema = mongoose.Schema({
-    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    content: { type: String, trim: true },
-    conversation: { type: mongoose.Schema.Types.ObjectId, ref: "Conversation" },
-    seenBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-});
+const messageSchema = mongoose.Schema(
+    {
+        sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+        content: { type: String, trim: true },
+        conversation: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Conversation",
+        },
+        seenBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    },
+    { timestamps: true }
+);
 
 const Message = mongoose.model("Message", messageSchema);
 module.exports = Message;
