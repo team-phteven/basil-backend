12832a09ab174d690cf03f3ab14638ffe9e7408f
rikifujihara <rikifujihara@gmail.com>
2022-11-08 12:34:38 +1100

    sort messages

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -6,6 +6,7 @@ const Conversation = require("../models/conversationModel");
 const getMessages = async (req, res) => {
     try {
         const messages = await Message.find({ chat: req.body.conversationId })
+            .sort({ createdAt: 1 })
             .populate("sender", "firstName avatar lastName")
             .populate("conversation");
         res.json(messages);
