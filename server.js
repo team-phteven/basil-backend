4e302c38237f34313dcd7dba757aa2cf6f504de2
rikifujihara <rikifujihara@gmail.com>
2022-11-10 18:39:03 +1100

    instant messages working but incorrect order

--- server.js
+++ server.js
@@ -20,15 +20,36 @@ const io = new Server(httpServer, {
 });
 
 io.on("connection", (socket) => {
-    console.log("connected to socket");
     socket.on("setup", (email) => {
         socket.join(email);
+        console.log("user joined socket on email ---> " + email);
         socket.emit("connected");
     });
 
     socket.on("join conversation", (conversation) => {
         socket.join(conversation);
         console.log("User joined conversation:" + conversation);
+    });
+
+    socket.on("new message", (newMessageReceived) => {
+        console.log(
+            "new message received in backend!! -> " + newMessageReceived
+        );
+        let conversation = newMessageReceived.conversation;
+
+        if (!conversation.users) return console.log("no users in the convo");
+
+        conversation.users.forEach((user) => {
+            if (user._id == newMessageReceived.sender._id) return;
+
+            socket.in(user.email).emit("message received", newMessageReceived);
+            console.log(
+                "new message received by:" +
+                    user.email +
+                    " :" +
+                    newMessageReceived.content
+            );
+        });
     });
 });
 
